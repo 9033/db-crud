ee29fee56cf4242bc714b5ea57ca4213ef19aae8
9033 <28668509+9033@users.noreply.github.com>
2020-05-31 17:00:08 +0900

    dynamodb 코드 test #1 시간 처리. (datetime, timestamp, dateonly등)

--- test.js
+++ test.js
@@ -74,6 +74,17 @@ const main2=async (init=false)=>{
     dynamodb 적용 계획.
         package table CRUD.
         trueuser table CRUD.
+2020-05-31:
+    datetime: start_date, end_date는 숫자. Date.parse()로 timestamp로 변환해서 넣는다.
+        아니면 toISOString으로 변환해서 넣는다. <- 알아보기 쉬운게 좋을듯.
+    dateonly: '2020-05-01'이런 식으로 문자열로 넣는다.
+    string이든 number든 내부적으로 각 데이터 타입에 맞게 정렬이 되니 마찬가지.
+    cf. iso time foramt.
+        Date.parse("2010-12-21T17:42:34Z"): without timezone
+        Date.parse("2010-12-21T17:42:34+09:00"): w tz
+        ex) get timestamp of "2020-06-01 KRT" -> Date.parse("2020-06-01T00:00+09:00"): must have 'hh:mm'
+    cf. 2
+        https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes
 */
 // cf. https://github.com/aws-samples/aws-nodejs-sample
 const AWS = require('aws-sdk');
@@ -94,8 +105,98 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 // get list
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+// docClient.scan({
+//     TableName: "images",
+//     // KeyConditionExpression:"",
+//     // Limit: 1,
+// }).promise().then(console.log)
+
+// table의 목록.
+// dynamodb.listTables().promise().then(console.log)
+
+// table의 성질
+// dynamodb.describeTable({
+//     TableName: "packages",
+// }).promise().then(r=>console.log(JSON.stringify(r, null, 2)))
+
+// table 만들기
+let params = {
+    AttributeDefinitions: [
+        {
+            AttributeName: "title",
+            AttributeType: "S",
+        },
+        {
+            AttributeName: "create_time",
+            AttributeType: "S",
+        }
+    ],
+    KeySchema: [
+        {
+            AttributeName: "title",
+            KeyType: "HASH",
+        },
+        {
+            AttributeName: "create_time",
+            KeyType: "RANGE",
+        }
+    ],
+    BillingMode: 'PAY_PER_REQUEST',
+    TableName: "packages"
+};
+// dynamodb.createTable(params).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+// dynamodb.updateTable(params).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+// dynamodb.deleteTable({
+//     TableName: "packages"
+// }).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+// dynamodb.describeTable({
+//     TableName: "packages",
+// }).promise().then(r=>console.log(JSON.stringify(r, null, 2)))
+const nowDate = new Date();
+// dynamodb.putItem({
+//     Item:{
+//         title:{
+//             S:'three kingdoms p 2'
+//         },
+//         create_time:{
+//             S:nowDate.toISOString(),
+//         },
+//         start_date:{
+//             N:Date.parse("2020-06-01").toString(),
+//         },
+//         end_date:{
+//             N:Date.parse("2020-09-01").toString(),
+//         },
+//         comment:{
+//             S:'comment',
+//         }
+//     },
+//     TableName: "packages"
+// }).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+// .finally(()=>{
+//     docClient.scan({
+//         TableName : 'packages',        
+//     }).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+// })
+
+// 구간 검색.
+/*
+putItem일때는 숫자를 문자열로.
+scan일때는 숫자는 숫자로.
+*/
+// docClient.scan({
+//     TableName : 'packages',
+//     FilterExpression : 'start_date BETWEEN :this_year AND :that_year',
+//     ExpressionAttributeValues : {
+//         ':this_year' : Date.parse('2020-05-02'),
+//         ':that_year' : Date.parse('2020-07-01'),
+//     },    
+// }).promise().then(r => console.log(JSON.stringify(r, null, 2)))
+
 docClient.scan({
-    TableName: "images",
-    // KeyConditionExpression:"",
-    Limit: 1,
-}).promise().then(console.log)
+    TableName : 'packages',
+    FilterExpression : ':to_date BETWEEN start_date AND end_date',
+    ExpressionAttributeValues : {
+        ':to_date' : Date.parse('2020-06-31'),
+    },    
+}).promise().then(r => console.log(JSON.stringify(r, null, 2)))
