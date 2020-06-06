d7686409d20c6781a7e3b1a549e97987b53e173d
9033 <28668509+9033@users.noreply.github.com>
2020-06-06 18:01:35 +0900

    sequelize, sqlite3 언인스톨. 의존도 있는 코드 제거. sequelize, sqlite3를 사용하는 코드도 제거.

--- test.js
+++ test.js
@@ -1,73 +1,3 @@
-const db=require('./models');
-
-const main=async (init=false)=>{//db초기화
-    if(init){/* db의 데이터를 초기화 */
-        let ret=await db.user.sync({force:true});
-        // db에 초기값 지정.
-        await db.user.create({
-            name:"한조"
-        });
-        const r = await db.user.findOne({where:{name:"한조"}})
-        const user_id=r.dataValues.id;
-
-
-        ret=await db.car.sync({force:true});
-        // db에 초기값 지정.
-
-        await db.car.create({
-            name:"마세라티",
-            user_id:r.dataValues.id,
-        })
-        await db.car.create({
-            name:"테슬라",
-            user_id:r.dataValues.id,
-        })
-
-
-    }
-    else{/* db의 데이터를 그대로 사용. */        
-        await db.user.sync();
-        await db.car.sync();
-    }
-
-    
-
-    const users=await db.user.findAll();
-    const cars=await db.car.findAll();
-
-    console.log(users.map(e=>e.dataValues));
-    console.log(cars.map(e=>e.dataValues));
-    
-    console.log(
-        JSON.stringify(await db.car.findAll({
-            // attributes:['name'],
-            include:[{                
-                model:db.user,
-                // attributes:['name'],
-            }]
-        }).map(e=>e.dataValues))
-    );
-    console.log(
-        JSON.stringify(await db.user.findAll({
-            // attributes:['name'],
-            include:[{                
-                model:db.car,
-                // attributes:['name'],
-            }]
-        }).map(e=>e.dataValues))
-    );
-    
-};
-// main();
-const main2=async (init=false)=>{
-    let r = await db.sequelize.query("select datetime('now'), datetime('now','localtime')"); 
-    console.log(r)
-    r = await db.sequelize.query("select date('now'), date('now','localtime')"); 
-    console.log(r)
-}
-
-// main2()
-
 /*
 2020-05-26:
     dynamodb 코드 test.
@@ -85,6 +15,8 @@ const main2=async (init=false)=>{
         ex) get timestamp of "2020-06-01 KRT" -> Date.parse("2020-06-01T00:00+09:00"): must have 'hh:mm'
     cf. 2
         https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes
+2020-06-06
+    dynamoDB로 CRUD적용 완료.
 */
 // cf. https://github.com/aws-samples/aws-nodejs-sample
 const AWS = require('aws-sdk');
